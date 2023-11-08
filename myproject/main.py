08aac39ccd531983ba35e0b0efb3fd0521d56e83
Aleksander-Baum <r0930134@student.thomasmore.be>
2023-11-08 21:10:24 +0100

    restaurant items finishes

--- myproject/main.py
+++ myproject/main.py
@@ -25,3 +25,15 @@ def get_db():
 @app.post("/restaurants", response_model=schemas.Restaurant)
 def create_restaurant(restaurant: schemas.RestaurantCreate, db: Session = Depends(get_db)):
     return crud.create_restaurant(db=db, restaurant=restaurant)
+
+@app.get("/restaurants/", response_model=list[schemas.Restaurant])
+def read_restaurants(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    restaurants = crud.get_restaurants(db, skip, limit)
+    return restaurants
+
+@app.get("/restaurants/{restaurant_id}", response_model=schemas.Restaurant)
+def read_restaurants(restaurant_id: int, db: Session = Depends(get_db)):
+    db_restaurant = crud.get_restaurant(db, restaurant_id=restaurant_id)
+    if db_restaurant is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_restaurant
